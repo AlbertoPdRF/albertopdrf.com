a6481b8a478313a617252f8c4fd45cc7ed588055
Alberto Pérez de Rada Fiol <apdrf.94@gmail.com>
2020-06-20 21:55:52 +0200

    Set up project structure and Material UI

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const App = () => (
+  <Fragment>
+    <CssBaseline />
+  </Fragment>
+);
+
+export default App;
