d73190cf14c1b7a6668d138d57d0d40eb82785ef
Alberto Pérez de Rada Fiol <apdrf.94@gmail.com>
2021-03-12 10:50:52 +0100

    Paginate the articles section
    Fixes #2

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Pagination from '@material-ui/lab/Pagination';
 
 import BreakpointMasonry from '../BreakpointMasonry/BreakpointMasonry';
 import ArticleCard from '../ArticleCard/ArticleCard';
@@ -24,30 +26,55 @@ const user = {
 const Articles = () => {
   const classes = useStyles();
 
+  const [allArticles, setAllArticles] = useState(null);
+  const [pagesCount, setPagesCount] = useState(0);
+  const [page, setPage] = useState(1);
   const [articles, setArticles] = useState(null);
 
   useEffect(() => {
     fetch(`https://dev.to/api/articles?username=${user.username}`)
       .then((response) => response.json())
-      .then((data) => setArticles(data));
+      .then((data) => {
+        setAllArticles(data);
+        setPagesCount(Math.ceil(data.length / 3));
+      });
   }, []);
+
+  useEffect(() => {
+    if (!allArticles) return;
+
+    setArticles(allArticles.filter(
+      (article, index) => index >= 3 * (page - 1) && index < 3 * page
+    ));
+  }, [allArticles, page]);
 
   return (
     <div className={classes.root}>
-      {articles && (
-        <BreakpointMasonry>
-          {articles.map((a) => (
-            <ArticleCard
-              key={a.slug}
-              title={a.title}
-              tags={a.tag_list}
-              coverImage={a.cover_image}
-              description={a.description}
-              url={a.canonical_url}
-            />
-          ))}
-        </BreakpointMasonry>
-      )}
+      <Grid container justify="center">
+        <Grid item>
+          {articles && (
+            <BreakpointMasonry>
+              {articles.map((a) => (
+                <ArticleCard
+                  key={a.slug}
+                  title={a.title}
+                  tags={a.tag_list}
+                  coverImage={a.cover_image}
+                  description={a.description}
+                  url={a.canonical_url}
+                />
+              ))}
+            </BreakpointMasonry>
+          )}
+        </Grid>
+        <Grid item>
+          <Pagination
+            count={pagesCount}
+            page={page}
+            onChange={(event, page) => setPage(page)}
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 };
